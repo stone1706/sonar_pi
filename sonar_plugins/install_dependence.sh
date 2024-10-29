fa92ccb68ce8faa4dddd3eb70d9ef822d63497b6
Twillpower <2118147923@qq.com>
2024-10-29 10:57:41 +0800

    sonar_pi

---
+++ sonar_plugins/install_dependence.sh
@@ -0,0 +1,15 @@
+sudo apt-get install libwxgtk3.0-gtk3-dev
+sudo apt-get install libgtk-3-dev
+sudo apt-get install wx3.0-headers libwxgtk3.0-dev
+sudo apt-get install libsqlite3-dev
+sudo apt-get install libshp-dev libtinyxml-dev libsndfile1-dev libcairo2-dev libportaudio2
+sudo apt-get install libbz2-dev
+sudo apt-get install libglew-dev
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install libcairo2-dev
+sudo apt-get install libpango1.0-dev
+sudo apt-get install libarchive-dev
+sudo apt install rapidjson-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libusb-1.0-0-dev
+sudo apt-get install libxrandr-dev
---
+++ sonar_plugins/src/Canvas.cpp
@@ -0,0 +1,121 @@
+/******************************************************************************
+ * $Id:   $
+ *
+ * Project:  OpenCPN
+ * Purpose:  Clickable canvas to draw upon
+ * Author:   Johan van der Sman
+ *
+ ***************************************************************************
+ *   Copyright (C) 2015 Johan van der Sman                                 *
+ *   johan.sman@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
+ ***************************************************************************
+ *
+ */
+//
+// wxWidgets sends the mouse movement and mouse clicks to the windows objects
+// In this case the panel object that acts as drawing canvas
+// This helper class receives the events and informs the radar view object that owns
+// the canvas
+//
+
+#include "wx/wxprec.h"
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif // precompiled headers
+
+#include "Canvas.h"
+
+BEGIN_EVENT_TABLE(Canvas, wxPanel)
+EVT_MOTION(Canvas::mouseMoved)
+EVT_LEFT_DOWN(Canvas::mouseDown)
+EVT_LEFT_UP(Canvas::mouseReleased)
+EVT_MOUSEWHEEL(Canvas::mouseScroll)
+EVT_PAINT(Canvas::paintEvent)
+// EVT_KEY_DOWN(Canvas::OnKeyDown) // 绑定按键事件
+END_EVENT_TABLE()
+
+// 重写键盘事件处理方法
+// void Canvas::OnKeyDown(wxKeyEvent &event)
+// {
+//     // if (event.GetKeyCode() == 'z')
+//     // {
+//     wxMessageBox("Hello, World!", "Canvas MessageBox", wxOK | wxICON_INFORMATION);
+//     // if (pPlugIn)
+//     // {
+//     //     pPlugIn->ToggleSonarFrame();
+//     // }
+//     // }
+//     event.Skip(); // 继续处理其他事件
+// }
+
+Canvas::Canvas(wxWindow *parent, SonarFrame *view,
+               wxWindowID id, const wxPoint &pos, const wxSize &size)
+    : wxPanel(parent, id, pos, size),
+      pv(view),
+      MouseDown(false),
+      Parent(view)
+{
+}
+
+void Canvas::mouseMoved(wxMouseEvent &event)
+{
+    if (MouseDown)
+    {
+        if (pv)
+        {
+            // pv->OnLeftMouse(event.GetPosition());
+            pv->OnLeftMouseMove(event.GetPosition());
+        }
+    }
+    event.Skip();
+}
+
+void Canvas::mouseDown(wxMouseEvent &event)
+{
+    MouseDown = true;
+    if (pv)
+    {
+        pv->OnLeftMouse(event.GetPosition());
+    }
+    event.Skip();
+}
+
+void Canvas::mouseReleased(wxMouseEvent &event)
+{
+    MouseDown = false;
+    if (pv)
+    {
+        pv->OnLeftMouseRelease(event.GetPosition());
+    }
+    event.Skip();
+}
+
+void Canvas::mouseScroll(wxMouseEvent &event)
+{
+    if (pv)
+    {
+        pv->OnMouseScroll(event.GetWheelRotation());
+    }
+    event.Skip();
+}
+
+void Canvas::paintEvent(wxPaintEvent &event)
+{
+    Parent->paintEvent(event);
+}
